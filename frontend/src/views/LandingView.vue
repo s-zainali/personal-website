fcaab9d8998eb2461033f369a019542243c5e76b
Syed Zain Ali <szainali284@gmail.com>
2026-07-30 00:11:08 +0500

    rerouted css

--- frontend/src/views/LandingView.vue
+++ frontend/src/views/LandingView.vue
@@ -1,6 +1,8 @@
 <template>
-    <div>
-        <h1 class="">My Image</h1>
-        <h1>Syed Zain Ali</h1>
+    <div class="flex justify-between gap-2 p-12">
+        <h1 class="text-5xl uppercase font-bold tracking-widest">Syed Zain Ali</h1>
+        <div class="w-120 rounded-full overflow-hidden shadow-xl border-4">
+            <img src="/profile.jpg" alt="My Image">
+        </div>
     </div>
 </template>
