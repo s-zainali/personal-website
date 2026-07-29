0b0b4aae1d4741c31d0e752b5eca7ed248b780a9
Syed Zain Ali <szainali284@gmail.com>
2026-07-30 00:05:27 +0500

    created landing view

---
+++ frontend/src/router/index.js
@@ -0,0 +1,12 @@
+import LandingView from '@/views/LandingView.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [{
+    path:'/',
+    component: LandingView
+  }],
+})
+
+export default router
---
+++ frontend/src/views/LandingView.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1 class="">My Image</h1>
+        <h1>Syed Zain Ali</h1>
+    </div>
+</template>
