404534b98a61481d237285b38b85086bf5a1d0c8
Syed Zain Ali <szainali284@gmail.com>
2026-07-30 19:48:50 +0500

    updated style

--- frontend/src/views/LandingView.vue
+++ frontend/src/views/LandingView.vue
@@ -1,8 +1,10 @@
 <template>
-    <div class="flex justify-between gap-2 p-12">
-        <h1 class="text-5xl uppercase font-bold tracking-widest">Syed Zain Ali</h1>
-        <div class="w-120 rounded-full overflow-hidden shadow-xl border-4">
-            <img src="/profile.jpg" alt="My Image">
+    <div class="w-full h-[100dvh] bg-ink-black-950">
+        <div class="flex justify-between gap-2 p-12">
+            <h1 class="text-7xl uppercase font-bold tracking-widest text-neon-ice-100">Syed Zain Ali</h1>
+            <div class="w-100 h-120 rounded-full overflow-hidden shadow-xl border-8 border-neon-ice-700">
+                <img class="h-full object-cover" src="/profile.jpg" alt="My Image">
+            </div>
         </div>
     </div>
 </template>
